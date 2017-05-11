805711379864a96faf031a512f3293fc42bd9feb
Hans Pabst <hans.pabst@intel.com>
2017-05-11 10:26:03 +0200

    Fixed issues with GFortran's built-in preprocessor, which seems to be even more limited than IFORT's preprocessor.

--- cp2k/src/base/base_uses.f90
+++ cp2k/src/base/base_uses.f90
@@ -38,12 +38,13 @@
 ! but deal elegantly with e.g. library wrapper routines that take arguments only used if the library is linked in. 
 ! This code should be valid for any Fortran variable, is always standard conforming,
 ! and will be optimized away completely by the compiler
+!
 #define MARK_USED(foo) IF(.FALSE.)THEN; DO ; IF(SIZE(SHAPE(foo))==-1) EXIT ;  END DO ; ENDIF
 
-! Macro which is calculating a single version number out of three components (major, minor, update).
-! This is helpful when writing preprocessor conditions for code depending on the compiler version.
-#define CP_VERSION3(MAJOR, MINOR, UPDATE) (MAJOR * 10000 + MINOR * 100 + UPDATE)
-#define CP_VERSION4(MAJOR, MINOR, UPDATE, PATCH) (MAJOR * 100000000 + MINOR * 1000000 + UPDATE * 10000 + PATCH)
+! Calculate version number from 3-components.
+#define CP_VERSION3(MAJOR, MINOR, UPDATE) ((MAJOR) * 10000 + (MINOR) * 100 + (UPDATE))
+! Calculate version number from 4-components.
+#define CP_VERSION4(MAJOR, MINOR, UPDATE, PATCH) ((MAJOR) * 100000000 + (MINOR) * 1000000 + (UPDATE) * 10000 + (PATCH))
 
 ! Evaluate the given symbol.
 #define CP_EVAL(A) A
@@ -51,23 +52,22 @@
 #define CP_CONCATENATE(A, B) CP_EVAL(CP_EVAL(A) ## CP_EVAL(B))
 
 ! The CP_CONTIGUOUS macro may (or may not) expand to the CONTIGUOUS attribute
-! depending on whether or not the compiler supports Fortran 2008.
-#if !defined(CP_DISABLE_ATTRIBS) && defined(__GFORTRAN__) && (CP_VERSION3(4, 6, 0) <= CP_VERSION3(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__))
+! depending on whether or not the compiler supports Fortran 2008. GNU Fortran
+! needs -std=f2008 to make CONTIGUOUS available, hence the CP_F2008 symbol
+! needs to be present (no way to detect standard by using the preprocessor).
+!
+#if !defined(CP_DISABLE_ATTRIBS) && defined(CP_F2008) && \
+  defined(__GFORTRAN__) && (CP_VERSION3(4, 6, 0) <= CP_VERSION3(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__))
 # define CP_CONTIGUOUS CONTIGUOUS
-#elif !defined(CP_DISABLE_ATTRIBS) && defined(__INTEL_COMPILER) && (1210 <= __INTEL_COMPILER)
+# define CP_COMMA_CONTIGUOUS , CONTIGUOUS
+#elif !defined(CP_DISABLE_ATTRIBS) && \
+  defined(__INTEL_COMPILER) && (1210 <= __INTEL_COMPILER)
 # define CP_CONTIGUOUS CONTIGUOUS
+# define CP_COMMA_CONTIGUOUS , CONTIGUOUS
 #else
 # define CP_CONTIGUOUS
+# define CP_COMMA_CONTIGUOUS
 #endif
-! Represents the comma eventually needed to attach the CONTIGUOUS attribute.
-! Not used directly but eventually expanded by CP_COMMA(CP_CONTIGUOUS).
-#define CP_COMMA_ATTRIB_CONTIGUOUS ,
-#define CP_COMMA_ATTRIB_
-! Expands to a comma eventually needed when using ATTRIB depending on
-! whether or not the ATTRIB is defined, otherwise expanded to nothing.
-#define CP_COMMA(ATTRIB) CP_EVAL(CP_CONCATENATE(CP_COMMA_ATTRIB, CP_CONCATENATE(_, ATTRIB)))
-! Provided for convenience; eventually expands to COMMA (,) and CONTIGUOUS.
-#define CP_COMMA_CONTIGUOUS CP_COMMA(CP_CONTIGUOUS) CP_CONTIGUOUS
 
 #if defined(__MKL)
 # define CP_MKL_PURE
